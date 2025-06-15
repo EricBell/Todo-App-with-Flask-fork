08730e1e274c503ad2422edebbc1cbb300f4d2a3
Eric Bell <eric@polymorph.co>
2025-06-15 07:41:13 -0400

    refine files

---
+++ run-tests-in-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+docker run --rm my-app ./run_tests.sh
--- zip-it.sh
+++ zip-it.sh
@@ -4,18 +4,22 @@
 
 case "$1" in
   preedit)
-    ZIPFILE="accessible-outings_preedit.zip"
+    ZIPFILE="preedit_codebase.zip"
     ;;
-  da)
-    ZIPFILE="accessible-outings_DA_postedit.zip"
+  beetle)
+    ZIPFILE="postedit_codebase_beetle.zip"
     ;;
   sonnet)
-    ZIPFILE="accessible-outings_Sonnet_postedit.zip"
+    ZIPFILE="postedit_codebase_sonnet.zip"
+    ;;
+  rewrite)
+    ZIPFILE="postedit_codebase_sonnet.zip"
     ;;
   *)
-    echo "Usage: $0 {preedit|da|sonnet}"
+    echo "Usage: $0 {preedit|beetle|sonnet|rewrite}"
     exit 1
     ;;
 esac
 
-zip -r "$ZIPFILE" . -x ".venv/*" ".venv" ".git/*" ".git" "*/__pycache__/*" "__pycache__" "zip-it.sh" ".env" "Suggestions.txt" "simple_unit_test.py"
+# zip -r "$ZIPFILE" . -x ".venv/*" ".venv" ".git/*" ".git" "*/__pycache__/*" "__pycache__" "zip-it.sh" "clean.sh" "tests/*" "tests"
+zip -r "$ZIPFILE" . -x "*.venv/*" "*.git/*" "*__pycache__/*" "*__pycache__" "zip-it.sh" "clean.sh" "tests/*" "tests" "run-tests-in-docker.sh"
